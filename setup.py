7739caa82f598919adbfdb7db50e13aad7d69544
Samyak <samyak.tamrakar@hrforecast.de>
2019-08-25 20:30:56 +0200

    Updated requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding = 'utf-
 setup(
 	name = 'GiantPandas',
 	packages = ['GiantPandas'],
-	version = '0.0',
+	version = '0.0.1',
 	license = 'MIT',
 	description = 'Some special functions and connectors for Pandas.',
 	long_description = long_description,
@@ -15,12 +15,21 @@ setup(
 	author = 'Samyak Ratna Tamrakar',
 	author_email = 'samyak.r.tamrakar@gmail.com',
 	url = 'https://github.com/srtamrakar/python-giant-pandas',
-	download_url = 'https://github.com/srtamrakar/python-giant-pandas/archive/v_0.0.tar.gz',
+	download_url = 'https://github.com/srtamrakar/python-giant-pandas/archive/v_0.0.1.tar.gz',
 	keywords = ['pandas', 'excel', 'postgres', 'psql', 'postgresql'],
 	install_requires = [
+		'numpy>=1.13.3',
+		'pandas>=0.25.0',
+		'pytest>=5.0.1',
+		'SQLAlchemy>=1.3.7',
+		'Unidecode>=1.0.22',
+		'xlrd>=1.2.0',
+		'XlsxWriter>=1.0.2',
+		'DailyLogger>=0.1.1',
+		'FreqObjectOps>=0.1.0',
 	],
 	classifiers = [
-		'Development Status :: 1 - Planning',  # Either"3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+		'Development Status :: 2 - Pre-Alpha',  # Either"3 - Alpha", "4 - Beta" or "5 - Production/Stable"
 		'Intended Audience :: Developers',  # Define that your audience are developers
 		'Topic :: Software Development :: Build Tools',
 		'License :: OSI Approved :: MIT License',
@@ -29,5 +38,5 @@ setup(
 		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7'
-	],
+	]
 )
